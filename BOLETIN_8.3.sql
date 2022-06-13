50194621bf029a199ba68dbb4486bdaa34559450
LuisVergaraCastro <48942067+LuisVergaraCastro@users.noreply.github.com>
2022-06-13 13:18:20 +0200

    Initial commit

---
+++ BOLETIN_8.3.sql
@@ -0,0 +1,21 @@
+--Boletín 8.3
+--Consultas sencillas
+
+--AdventureWorks
+
+--1.Nombre del producto, código y precio, ordenado de mayor a menor precio
+
+--2.Número de direcciones de cada Estado/Provincia
+
+--3.Nombre del producto, código, número, tamaño y peso de los productos
+--que estaban a la venta durante todo el mes de septiembre de 2002.
+--No queremos que aparezcan aquellos cuyo peso sea superior a 2000.
+
+--4.Margen de beneficio de cada producto (Precio de venta menos el coste), y porcentaje que supone respecto del precio de venta.
+
+
+--Consultas de dificultad media
+
+--5.Número de productos de cada categoría
+
+--6.cantidad de subcategorías que hay dentro de cada categoría padre
